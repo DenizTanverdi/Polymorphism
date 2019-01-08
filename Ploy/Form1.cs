dcc6fe4b27c97aad56fe1ffc9e0fa687efd5cef8
semvs <semvs@ayvansaray.edu.tr>
2019-01-08 11:23:49 +0300

    Add project files.

---
+++ Ploy/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static Ploy.MyClasses;
+
+namespace Ploy
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        List<DortgenKutu> dK = new List<DortgenKutu>();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DortgenKutu kutu = new DortgenKutu();
+            kutu.Id = 1;
+            kutu.UrunAd = "Karton Kutu";
+            kutu.KisaKenar = 10;
+            kutu.UzunKenar = 15;
+            kutu.Yukseklik = 20;
+            dK.Add(kutu);
+            kutu = new DortgenKutu();
+            kutu.Id = 2;
+            kutu.UrunAd = "Mukavva Kutu";
+            kutu.KisaKenar = 15;
+            kutu.UzunKenar = 10;
+            kutu.Yukseklik = 25;
+            dK.Add(kutu);
+            kutu = new DortgenKutu();
+            kutu.Id = 3;
+            kutu.UrunAd = "Karton Kutu";
+            kutu.KisaKenar = 17;
+            kutu.UzunKenar = 17;
+            kutu.Yukseklik = 27;
+            dK.Add(kutu);
+            kutu = new DortgenKutu();
+            kutu.Id = 4;
+            kutu.UrunAd = "Karton Kutu";
+            kutu.KisaKenar = 18;
+            kutu.UzunKenar = 30;
+            kutu.Yukseklik = 28;
+            dK.Add(kutu);
+
+            dataGridView1.DataSource = dK;
+        }
+    }
+}
---
+++ Ploy/MyClasses.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ploy
+{
+    class MyClasses
+    {
+        public class Urun
+        {
+            public int Id { get; set; }
+            public string UrunAd { get; set; }
+            public int Yukseklik { get; set; }
+            public virtual double Hacim()
+            {
+                double hacim = Math.Pow(Convert.ToDouble(Yukseklik), 3);
+                return Convert.ToInt32(hacim);
+            }
+            public int Fire()
+            {
+                return Convert.ToInt32(this.Hacim() * 0.20f);
+            }
+        }
+        public class KupuKutu:Urun
+        {
+
+        }
+        public class DortgenKutu : Urun
+        {
+            public int KisaKenar { get; set; }
+            public int UzunKenar { get; set; }
+            public override double Hacim()
+            {
+                return Yukseklik*KisaKenar*UzunKenar;
+            }
+        }
+        public   class Silindir:Urun
+        {
+            private int yariCap;
+
+            public int YariCap
+            {
+                get { return yariCap; }
+                set { yariCap = value; }
+            }
+            public override double Hacim()
+            {
+                return Math.PI*Math.Pow(yariCap,2)*Yukseklik;    
+            }
+
+        }
+    }
+}
